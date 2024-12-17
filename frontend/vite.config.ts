7363da5306405df63684a1294d16572bfa960aef
Christoffer <christoffer.k.friman@gmail.com>
2024-12-17 17:25:03 +0100

    fix: github action bugs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dns from "dns";
+import path from "path";
 dns.setDefaultResultOrder("ipv4first");
 
 // https://vite.dev/config/
@@ -19,6 +20,11 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     host: true,
     port: 5173,
