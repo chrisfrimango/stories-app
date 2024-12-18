53227f49327705696fc8f823d04e8837dbcbbed5
Christoffer <christoffer.k.friman@gmail.com>
2024-12-18 12:16:04 +0100

    fix: WIP fix import bug

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dns from "dns";
-import path from "path";
+// import path from "path";
 dns.setDefaultResultOrder("ipv4first");
 
 // https://vite.dev/config/
@@ -20,14 +20,18 @@ export default defineConfig({
       },
     }),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      src: path.resolve(__dirname, "./src"),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //     src: path.resolve(__dirname, "./src"),
+  //   },
+  // },
   server: {
     host: true,
     port: 5173,
+    fs: {
+      allow: [".."],
+      cachedChecks: false,
+    },
   },
 });
